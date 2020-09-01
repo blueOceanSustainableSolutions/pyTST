1d6023161249de36ffa0b6bfabb5fc66fbf95c51
mKlapwijk <m.d.klapwijk@hotmail.com>
2020-09-01 16:22:24 +0200

    fix to prevent recursive spawning in windows

--- example/example.py
+++ example/example.py
@@ -11,7 +11,9 @@ tst = pyTST()
 
 tst.load_data_file("example_data_filename", signal_column=1, time_column=0, tstep=0.05)
 
-tst.compute_TST(step_size=4)
+# fix to prevent recursive spawning under Windows
+if __name__ == '__main__':
+  tst.compute_TST(step_size=4)
 tst.plot()
 
 # Plot input signal data
