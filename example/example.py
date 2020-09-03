a9743b4500bc36a7ce30744856e44b7cdcdfc37c
mKlapwijk <48097248+mKlapwijk@users.noreply.github.com>
2020-09-03 16:52:07 +0200

    fix again
    Properly tested on windows 10, works from command prompt

--- example/example.py
+++ example/example.py
@@ -6,25 +6,23 @@ from matplotlib import pyplot
 from pyTST import pyTST
 
 
-tst = pyTST()
-
-
-tst.load_data_file("example_data_filename", signal_column=1, time_column=0, tstep=0.05)
-
 # fix to prevent recursive spawning under Windows
 if __name__ == '__main__':
+    tst = pyTST()
+    tst.load_data_file("example_data_filename", signal_column=1, time_column=0, tstep=0.05)
+
     tst.compute_TST(step_size=4)
-tst.plot()
+    tst.plot()
 
-# Plot input signal data
-pyplot.figure()
-idx = 332 # result of tst analysis
+    # Plot input signal data
+    pyplot.figure()
+    idx = 332 # result of tst analysis
 
-pyplot.axvline(tst.time_array[idx], color='k', lw=0.8, ls='--', alpha=0.6)
-pyplot.plot(tst.time_array[0:idx], tst.signal_array[0:idx], color='C1', alpha=0.8)
-pyplot.plot(tst.time_array[idx:], tst.signal_array[idx:], color='C0')
+    pyplot.axvline(tst.time_array[idx], color='k', lw=0.8, ls='--', alpha=0.6)
+    pyplot.plot(tst.time_array[0:idx], tst.signal_array[0:idx], color='C1', alpha=0.8)
+    pyplot.plot(tst.time_array[idx:], tst.signal_array[idx:], color='C0')
 
-pyplot.xlabel("t")
-pyplot.ylabel("signal")
-pyplot.show()
+    pyplot.xlabel("t")
+    pyplot.ylabel("signal")
+    pyplot.show()
 
