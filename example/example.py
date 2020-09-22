acfabf8fad1fb9af7a7cce5583851df67c535298
mKlapwijk <m.d.klapwijk@hotmail.com>
2020-09-22 14:23:13 +0200

    modifications after review by SL

--- example/example.py
+++ example/example.py
@@ -5,7 +5,7 @@ import numpy as np
 from matplotlib import pyplot
 from pyTST import pyTST
 
-selectTimeDiscard = True
+select_time_discard = True
 step_size = 20
 
 
@@ -15,8 +15,8 @@ if __name__ == '__main__':
     tst.load_data_file("example_data_filename", signal_column=1, time_column=0, tstep=0.05)
 
     tst.compute_TST(step_size=step_size)
-    tst.plot(filename='TST_example',fileFormat='.eps',\
-                    selectTimeDiscard=selectTimeDiscard,step_size=step_size)
+    tst.plot(fileprefix='TST_example',fileformat='.eps',\
+                    select_time_discard=select_time_discard,step_size=step_size)
 
 # EOF
 
--- example/exampleSRS.py
+++ example/exampleSRS.py
@@ -5,7 +5,7 @@ import numpy as np
 from matplotlib import pyplot
 from pyTST import pyTST
 
-selectTimeDiscard = True
+select_time_discard = True
 step_size = 500
 
 
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     tst.load_data_file("SRSexample_data_file", signal_column=0, time_column=None, tstep=0.05)
 
     tst.compute_TST(step_size=step_size)
-    tst.plot(filename='TST_exampleSRS',fileFormat='.eps',\
-                    selectTimeDiscard=selectTimeDiscard,step_size=step_size)
+    tst.plot(fileprefix='TST_exampleSRS',fileformat='.eps',\
+                    select_time_discard=select_time_discard,step_size=step_size)
 
 # EOF
